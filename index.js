1a602dd41f92a7e85ea4501c62e7ce54566cd44c
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-07-08 13:11:37 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const server = express()
+const PORT = process.env.PORT || 5000
+server.get('/api', (req, res) => {
+    res.json({
+        message: 'web43 is working and ready to serve'
+    })
+})
+server.listen(PORT, () => {
+    console.log(`I am listening at ${PORT}`)
+})
