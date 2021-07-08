e21c567fefe8c8300476667e274de31ed2f06e69
wzd200 <william.z.dye@gmail.com>
2021-07-08 14:02:16 -0400

    Created react app and hooked it up

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 require('dotenv').config()
+const cors = require('cors')
+const path = require('path')
 const express = require('express')
 const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join('__dirname, client/')))
 const PORT = process.env.PORT || 5000
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
 server.get('/api', (req, res) => {
     res.json({
         message: 'web43 is working and ready to serve'
+    })
+})
+server.use((req, res) => {
+    res.json({
+        message: 'Sorry, could not serve'
     })
 })
 server.listen(PORT, () => {
